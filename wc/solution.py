f33a604ab0d0bb1d6c5e1a5f2e6913fc92a02353
EmanuelaMollova <emanuela.mollova@gmail.com>
2014-03-24 00:08:07 +0200

    Add solution and tests for wc

---
+++ wc/solution.py
@@ -0,0 +1,8 @@
+def wc():
+    pass
+
+def main():
+    print(wc())
+
+if __name__ == '__main__':
+    main()
---
+++ wc/tests.py
@@ -0,0 +1,16 @@
+import unittest
+from solution import wc
+
+
+class WcTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_wc(self):
+        self.assertEqual(, wc())
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
