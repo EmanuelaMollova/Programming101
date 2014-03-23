18241548a1b43ec5e50ed1260e2223292bb058a7
EmanuelaMollova <emanuela.mollova@gmail.com>
2014-03-24 00:08:07 +0200

    Add solution and tests for generate_numbers

---
+++ generate_numbers/solution.py
@@ -0,0 +1,8 @@
+def generate_numbers():
+    pass
+
+def main():
+    print(generate_numbers())
+
+if __name__ == '__main__':
+    main()
---
+++ generate_numbers/tests.py
@@ -0,0 +1,16 @@
+import unittest
+from solution import generate_numbers
+
+
+class GenerateNumbersTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_generate_numbers(self):
+        self.assertEqual(, generate_numbers())
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
