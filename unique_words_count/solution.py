0b842ddcade3c9faceb6bc7afb6d92d1600756db
EmanuelaMollova <emanuela.mollova@gmail.com>
2014-03-24 00:08:07 +0200

    Add solution and tests for unique_words_count

---
+++ unique_words_count/solution.py
@@ -0,0 +1,13 @@
+# Solution 1
+def unique_words_count(arr):
+    return len(set(arr))
+
+
+# Solution 2
+# def unique_words_count(arr):
+#     unique = []
+#     for item in arr:
+#         if item not in unique:
+#             unique.append(item)
+
+#     return len(unique)
