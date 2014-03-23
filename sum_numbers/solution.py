f12880dbbdeff68ddda8ec325fcc3a4ce885f0d3
EmanuelaMollova <emanuela.mollova@gmail.com>
2014-03-24 00:08:08 +0200

    Add solution and tests for sum_numbers

---
+++ sum_numbers/solution.py
@@ -0,0 +1,8 @@
+def sum_numbers():
+    pass
+
+def main():
+    print(sum_numbers())
+
+if __name__ == '__main__':
+    main()
---
+++ sum_numbers/tests.py
@@ -0,0 +1,16 @@
+import unittest
+from solution import sum_numbers
+
+
+class SumNumbersTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_sum_numbers(self):
+        self.assertEqual(, sum_numbers())
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
