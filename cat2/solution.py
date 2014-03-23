687b390db10097ae9c5cd4bb1be21ec3f466c2b1
EmanuelaMollova <emanuela.mollova@gmail.com>
2014-03-24 00:08:08 +0200

    Add solution and tests for cat2

---
+++ cat2/solution.py
@@ -0,0 +1,8 @@
+def cat2():
+    pass
+
+def main():
+    print(cat2())
+
+if __name__ == '__main__':
+    main()
---
+++ cat2/tests.py
@@ -0,0 +1,16 @@
+import unittest
+from solution import cat2
+
+
+class Cat2Test(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_cat2(self):
+        self.assertEqual(, cat2())
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
