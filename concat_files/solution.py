e8d90e1656d0c3b03b95c2db14b3614984df7c7f
EmanuelaMollova <emanuela.mollova@gmail.com>
2014-03-24 00:08:08 +0200

    Add solution and tests for concat_files

---
+++ concat_files/solution.py
@@ -0,0 +1,8 @@
+def concat_files():
+    pass
+
+def main():
+    print(concat_files())
+
+if __name__ == '__main__':
+    main()
---
+++ concat_files/tests.py
@@ -0,0 +1,16 @@
+import unittest
+from solution import concat_files
+
+
+class ConcatFilesTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_concat_files(self):
+        self.assertEqual(, concat_files())
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
