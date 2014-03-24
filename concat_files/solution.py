19742e1e109e80fcc4816d179d02cd0b0de512ac
EmanuelaMollova <emanuela.mollova@gmail.com>
2014-03-25 00:43:01 +0200

    Improve a solution

--- concat_files/solution.py
+++ concat_files/solution.py
@@ -1,18 +1,26 @@
 import sys
 
-def main():
-    if len(sys.argv) > 1:
+
+def concat_files(argv):
+    if len(argv) > 1:
         megatron = open("MEGATRON", "a+")
 
         content = ""
-        for i in range(1, len(sys.argv)):
+        for i in range(1, len(argv)):
             file = open(sys.argv[i], "r")
             content += file.read() + "\n"
             file.close()
+
         megatron.write(content)
         megatron.close()
+
+        return content
     else:
-        print("Please give at least one filename.")
+        return "Please give at least one filename."
+
+
+def main():
+    print(concat_files(sys.argv))
 
 if __name__ == '__main__':
     main()
